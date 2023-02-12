f5ed2c2003f547e8fd4c8aa4d49822bdcd8efd1a
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2023-02-12 20:59:11 +0100

    setup link with dev mode

--- src/textures/matcaps.ts
+++ src/textures/matcaps.ts
@@ -4,5 +4,5 @@ const textureFilenames = ["1", "2", "3", "4", "5", "6", "7", "8"] as const;
 export type TextureFilenames = typeof textureFilenames[number];
 
 export const matcaps = textureFilenames.map((filename) =>
-  textureLoader.load(getUrl(`textures/matcaps/${filename}.png`))
+  textureLoader.load(getUrl(`/textures/matcaps/${filename}.png`))
 );
--- src/utils/getUrl.ts
+++ src/utils/getUrl.ts
@@ -1,6 +1,5 @@
 export function getUrl(url: string) {
-  // get from .env file base url
-  const baseUrl =
-    "https://wleklinskimateusz.github.io/three-journey-01-basics/";
-  return baseUrl ? baseUrl + url : url;
+  return process.env.NODE_ENV === "development"
+    ? url
+    : "https://wleklinskimateusz.github.io/three-journey-01-basics" + url;
 }
